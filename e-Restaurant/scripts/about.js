d7c0e483e7a0c925642f8ecd17298eadabb5105a
belenirican <116881200+belenirican@users.noreply.github.com>
2022-12-15 14:28:12 +0300

    Add files via upload

---
+++ e-Restaurant/scripts/about.js
@@ -0,0 +1,11 @@
+
+/*
+let active_item = document.querySelector(".active");
+let body = document.querySelector("body");
+
+$('#testimonial').on('slide.bs.carousel'), function(e){
+    
+    if(e.relatedTarget == 1)
+        body.style.backgroundImage = "url('../assets/images/place0.jpg')";
+}
+*/
